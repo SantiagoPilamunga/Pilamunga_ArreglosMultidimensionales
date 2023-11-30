b6890cf571ef8249059039765da8951de19e5e9b
Santiago <santiago.pilamunga@udla.edu.ec>
2023-11-30 16:11:39 -0500

    Declaracion de columnas filas y #de matrices

--- ejercicio.c
+++ ejercicio.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    
+    int l,n,m;
+    printf("Ingrese el numero de matrices: ");
+    scanf("%d", &l);
+    printf("Ingrese el numero de filas: ");
+    scanf("%d", &n);
+    printf("Ingrese el mumero de columnas: ");
+    scanf("%d", &m);
     return 0;
 }
