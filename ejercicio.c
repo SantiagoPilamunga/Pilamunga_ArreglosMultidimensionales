5115796f9af51cf8580b3123388d997f78278afa
Santiago <santiago.pilamunga@udla.edu.ec>
2023-11-30 16:44:41 -0500

    Utilizamos alt+shift+F para que se vea mejor

--- ejercicio.c
+++ ejercicio.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int l,n,m;
+    int l, n, m;
     printf("Ingrese el numero de matrices: ");
     scanf("%d", &l);
     printf("Ingrese el numero de filas: ");
@@ -11,33 +11,34 @@ int main()
     int matrix[l][n][m];
     for (int k = 0; k < l; k++)
     {
-        if (k==l-1)
+        if (k == l - 1)
         {
             printf("Matriz final: \n");
             for (int i = 0; i < n; i++)
             {
-            for (int j = 0; j < m; j++)
-                {
-                    matrix[k][i][j]=1;
-                    printf("%d\t", matrix[k][i][j]);
-                }
-            printf("\n");
-            }
-        }else{
-
-            printf("Matriz: %d \n", k+1);
-            for (int i = 0; i < n; i++)
-            {
                 for (int j = 0; j < m; j++)
                 {
-                    matrix[k][i][j]=0;
+                    matrix[k][i][j] = 1;
                     printf("%d\t", matrix[k][i][j]);
                 }
                 printf("\n");
             }
+        }
+        else
+        {
 
+            printf("Matriz: %d \n", k + 1);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[k][i][j] = 0;
+                    printf("%d\t", matrix[k][i][j]);
+                }
+                printf("\n");
+            }
         }
     }
-    
+
     return 0;
 }
